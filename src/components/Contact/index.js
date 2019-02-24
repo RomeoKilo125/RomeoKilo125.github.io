31ac1823a7f984810e9b77efc24189bf045dfe11
RomeoKilo125 <romeokilo125@gmail.com>
2019-02-24 10:47:10 -0500

    contact panel

---
+++ src/components/Contact/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import "./style.css"
+
+function Contact() {
+  return (<div className='contact contact-grid'>
+    <div className='contact contact-item'>
+      <a href='https://twitter.com/RomeoKilo125' target='blank'>
+        <i class="fab fa-twitter"></i>
+      </a>
+    </div>
+    <div className='contact contact-item'>
+      <a href='https://github.com/RomeoKilo125' target='blank'>
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <div className='contact contact-item'>
+      <a href='https://linkedin.com/in/romeokilo125/' target='blank'>
+        <i class="fab fa-linkedin-in"></i>
+      </a>
+    </div>
+    <div></div>
+    <div className='contact contact-item'>
+      <a href='mailto:RomeoKilo125@gmail.com' target='blank'>
+        <i class="fas fa-envelope"></i>
+      </a>
+    </div>
+  </div>)
+}
+
+export default Contact;
