94f99fd59cc0a2a2207b7fc5d34e9c1daea2579a
RomeoKilo125 <romeokilo125@gmail.com>
2019-02-24 11:14:56 -0500

    added header bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Contact from './components/Contact'
+import Header from './components/Header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Contact />
+        <Header />
       </div>
     );
   }
---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+function Header() {
+  return (
+    <div className='header header-bar'>
+      <h1 className='header header-item'>RomeoKilo125</h1>
+      <h3 className='header header-item'>C. Ross W. King</h3>
+    </div>)
+}
+
+export default Header
