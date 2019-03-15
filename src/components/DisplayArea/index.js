c41d0acd84481bd55b72af27c2f6bbc2f3205b4b
RomeoKilo125 <romeokilo125@gmail.com>
2019-03-15 19:56:51 -0400

    changed display area to a stateless component

--- src/components/DisplayArea/index.js
+++ src/components/DisplayArea/index.js
@@ -1,12 +1,20 @@
-import React, {Component} from 'react'
+import React from 'react'
 import './style.css'
 
-class DisplayArea extends Component {
-render() {
+function DisplayArea(props) {
+  let item = props.item
   return (
-    <div className='display'>{JSON.stringify(this.props.item)}</div>
-  )
-}
+    item.name === 'About'
+    ? <div className='display display-about'>
+      Bio: {item.bio}
+    </div>
+    : <div className='display display-project'>
+      <div className='project-name'>{item.name}</div>
+      <img alt='Project Thumbnail' className='project-thumbnail' src={item.thumbnail}></img>
+      <div className='project-description'>{item.description}</div>
+      <div className='project-link'>Code: <a href={item.codeLink} > {item.codeLink}</a></div>
+      {item.liveLink !== "" ? <div className='project-link'>Live: <a href={item.liveLink} > {item.liveLink}</a></div> :''}
+    </div>)
 }
 
 export default DisplayArea
