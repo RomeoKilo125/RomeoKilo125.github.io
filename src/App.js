ff17b8605259949e790da34394cc40ff69a348dc
RomeoKilo125 <romeokilo125@gmail.com>
2019-04-12 16:32:19 -0400

    moved portfolio to a component of its own

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
-import About from './data/about.json'
-import Projects from './data/projects.json'
-import Contact from './components/Contact'
-import Header from './components/Header'
-import Navbar from './components/Navbar'
-import NavItem from './components/NavItem'
-
-import DisplayArea from './components/DisplayArea'
+import Portfolio from './components/Portfolio'
 
 class App extends Component {
-  state = {
-    'items': Projects,
-    'activeItem': ''
-  }
-
-  componentDidMount() {
-    this.setState({activeItem: About})
-  }
-
-  handleClick = (item) => {
-    console.log("click")
-    this.setState({activeItem: item})
-  }
-
   render() {
-    return (<div className="App">
-      <Contact/>
-      <Header/>
-      <Navbar>
-        <NavItem handleClick={this.handleClick} id="about-tag" item={About} className={About === this.state.activeItem
-          ? 'nav nav-item about-item active-item'
-          : 'nav nav-item about-item'}/> {
-            this.state.items.map(e => <NavItem handleClick={this.handleClick} key={this.state.items.indexOf(e)} item={e} className={e === this.state.activeItem
-              ? 'nav nav-item active-item'
-              : 'nav nav-item'}/>)
-        }
-      </Navbar>
-      <DisplayArea item={this.state.activeItem}/>
-    </div>);
+    return (<Portfolio/>)
   }
+
 }
 
 export default App;
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import './style.css';
+import About from '../../data/about.json'
+import Projects from '../../data/projects.json'
+import Contact from '../Contact'
+import Header from '../Header'
+import Navbar from '../Navbar'
+import NavItem from '../NavItem'
+import DisplayArea from '../DisplayArea'
+
+class Portfolio extends Component {
+  state = {
+    'items': Projects,
+    'activeItem': ''
+  }
+
+  componentDidMount() {
+    this.setState({activeItem: About})
+  }
+
+  handleClick = (item) => {
+    console.log("click")
+    this.setState({activeItem: item})
+  }
+
+  render() {
+    return (<div className="Portfolio">
+      <Contact/>
+      <Header/>
+      <Navbar>
+        <NavItem handleClick={this.handleClick} id="about-tag" item={About} className={About === this.state.activeItem
+          ? 'nav nav-item about-item active-item'
+          : 'nav nav-item about-item'}/> {
+            this.state.items.map(e => <NavItem handleClick={this.handleClick} key={this.state.items.indexOf(e)} item={e} className={e === this.state.activeItem
+              ? 'nav nav-item active-item'
+              : 'nav nav-item'}/>)
+        }
+      </Navbar>
+      <DisplayArea item={this.state.activeItem}/>
+    </div>);
+  }
+}
+
+export default Portfolio;
