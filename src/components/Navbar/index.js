c25de115d395d9da81bf7ce37b1a0521e22bf2fb
RomeoKilo125 <romeokilo125@gmail.com>
2019-04-13 23:37:53 -0400

    hiding and showing menu

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,27 @@
 import React, {Component} from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faBars} from '@fortawesome/free-solid-svg-icons'
+
 import './style.css'
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      visible: 'off'
+    }
+  }
+
+  navToggle = () => {
+    this.setState({visible: this.state.visible === 'on' ? 'off' : 'on'})
+  }
+
   render() {
-    return (
-      <div className='nav navbar'>{this.props.children}</div>)
+    let nav_class = `nav navbar ${this.state.visible}`
+    return (<div>
+      <FontAwesomeIcon icon={faBars} className='nav-icon' onClick={this.navToggle}/>
+      <div className={nav_class} onClick={this.navToggle}>{this.props.children}</div>
+    </div>)
   }
 }
 
