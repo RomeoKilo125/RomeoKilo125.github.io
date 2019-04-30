b1ebb88abc6bc619960c5a4da45447f9f86504af
RomeoKilo125 <romeokilo125@gmail.com>
2019-04-30 10:35:17 -0400

    Updates

---
+++ precache-manifest.a8a492a08830a7b709bb963445f9a073.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "91b06f59f468af0f0e37",
+    "url": "/static/css/main.192c5a7f.chunk.css"
+  },
+  {
+    "revision": "91b06f59f468af0f0e37",
+    "url": "/static/js/main.93f840da.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2f339de949d8b4d3e9b6",
+    "url": "/static/js/2.ac682e9d.chunk.js"
+  },
+  {
+    "revision": "60c1f228e563477d3411cce74b710946",
+    "url": "/index.html"
+  }
+];
