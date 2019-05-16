f28bc88d5151e88bb8d96c37e1aa7ac0aff928ad
RomeoKilo125 <romeokilo125@gmail.com>
2019-05-16 12:30:59 -0400

    added hangman back into project lineup

--- src/data/project-staging.js
+++ src/data/project-staging.js
@@ -5,10 +5,3 @@
 //  "description": "A scraper for the NY Times website",
 //  "thumbnail": "http://placehold.jp/24/cc9999/993333/500x500.png"
 // },
-// {
-//   "name": "Hangman",
-//   "codeLink": "https://github.com/RomeoKilo125/hangman-game",
-//   "liveLink": "https://RomeoKilo125.github.io/hangman-game",
-//   "description": "A basic hangman game",
-//   "thumbnail": "http://placehold.jp/24/cc9999/993333/500x500.png"
-// },
