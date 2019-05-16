f9625831332b4bf81df663bdd416c8caf6b8a0cd
RomeoKilo125 <romeokilo125@gmail.com>
2019-05-16 12:37:13 -0400

    removed node search bots temporarily until repairs can be effected

--- src/data/project-staging.js
+++ src/data/project-staging.js
@@ -5,3 +5,10 @@
 //  "description": "A scraper for the NY Times website",
 //  "thumbnail": "http://placehold.jp/24/cc9999/993333/500x500.png"
 // },
+// {
+//   "name": "Node Language Recognition",
+//   "codeLink": "https://github.com/RomeoKilo125/liri",
+//   "liveLink": "",
+//   "description": "A basic twitter bot",
+//   "thumbnail": "http://placehold.jp/24/cc9999/993333/500x500.png"
+// },
