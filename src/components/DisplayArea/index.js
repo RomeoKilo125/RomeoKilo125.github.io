84f83f28dfa2e596892529269b420d50ab147532
RomeoKilo125 <romeokilo125@gmail.com>
2020-01-09 19:09:02 -0500

    made JS Standard compliant

--- src/components/DisplayArea/index.js
+++ src/components/DisplayArea/index.js
@@ -1,35 +1,33 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import ReactPlayer from 'react-player'
 import './style.css'
 
 class DisplayArea extends Component {
-
-  render() {
+  render () {
     return (
       this.props.item.name === 'About'
-      ? <div className='display display-about'>
-        <div className='profilePic'>
-          <img className='about-img' alt='Profile Pic' src={process.env.PUBLIC_URL + this.props.item.pic}/>
+        ? <div className='display display-about'>
+          <div className='profilePic'>
+            <img className='about-img' alt='Profile Pic' src={process.env.PUBLIC_URL + this.props.item.pic}/>
+          </div>
+          <div className='bio'>{this.props.item.bio}</div>
         </div>
-        <div className='bio'>{this.props.item.bio}</div>
-      </div>
-      : <div className='display display-project'>
-        <div className='project-name'>{this.props.item.name}</div>
-        <div className='project-thumbnail'>
-          <ReactPlayer className='embedded-video' url={this.props.item.thumbnail} controls="controls" width='100%' height='100%'/>
-        </div>
-        <div className='project-description'>{this.props.item.description}</div>
-        <div className='project-link'>
-          <a target="blank" href={this.props.item.codeLink}>Code Repo</a>
-        </div>
-        {
-          this.props.item.liveLink !== ""
-            ? <div className='project-link'><a target="blank" href={this.props.item.liveLink}>Live Project</a>
-            </div>
-            : ''
-        }
-      </div>)
-
+        : <div className='display display-project'>
+          <div className='project-name'>{this.props.item.name}</div>
+          <div className='project-thumbnail'>
+            <ReactPlayer className='embedded-video' url={this.props.item.thumbnail} controls="controls" width='100%' height='100%'/>
+          </div>
+          <div className='project-description'>{this.props.item.description}</div>
+          <div className='project-link'>
+            <a target="blank" href={this.props.item.codeLink}>Code Repo</a>
+          </div>
+          {
+            this.props.item.liveLink !== ''
+              ? <div className='project-link'><a target="blank" href={this.props.item.liveLink}>Live Project</a>
+              </div>
+              : ''
+          }
+        </div>)
   }
 }
 
