b6ab5484409442d2a53ff9e71f5cba99aeb4c83a
RomeoKilo125 <romeokilo125@gmail.com>
2021-08-25 00:00:34 -0400

    updated profile pic and about me section

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,23 +4,23 @@ import './style.css'
 function Contact() {
   return (<div className='contact contact-grid'>
     <div className='contact contact-item'>
-      <a href='https://twitter.com/RomeoKilo125' target='blank'>
+      <a href='https://twitter.com/RomeoKilo125' target='_blank' rel='noreferrer'>
         <i className="fab fa-twitter"></i>
       </a>
     </div>
     <div className='contact contact-item'>
-      <a href='https://github.com/RomeoKilo125' target='blank'>
+      <a href='https://github.com/RomeoKilo125' target='_blank' rel='noreferrer'>
         <i className="fab fa-github"></i>
       </a>
     </div>
     <div className='contact contact-item'>
-      <a href='https://linkedin.com/in/romeokilo125/' target='blank'>
+      <a href='https://linkedin.com/in/romeokilo125/' target='_blank' rel='noreferrer'>
         <i className="fab fa-linkedin-in"></i>
       </a>
     </div>
     <div className='emptySpace'></div>
     <div className='contact contact-item'>
-      <a href='mailto:RomeoKilo125@gmail.com' target='blank'>
+      <a href='mailto:RomeoKilo125@gmail.com' target='_blank' rel='noreferrer'>
         <i className="fas fa-envelope"></i>
       </a>
     </div>
